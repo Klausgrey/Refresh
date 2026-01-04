99536f2e5b7291d8240356de88172a98656ccd12
Nicholas <danielnick210@gmail.com>
2026-01-04 09:43:32 +0100

    this is my demo  repo

---
+++ refresher.js
@@ -0,0 +1,65 @@
+// let hisName = "Nick";
+// const name2 = "Zamani";
+
+
+// const obj = {
+// 	firstName: "Charles",
+// 	lastName: "Daniel",
+// 	age: 15,
+// 	number: "08122168624"
+// }
+
+// const listedArray = [
+// 	{id: 1, firstName: "Nicholas", age:30, address: "Ogar street"},
+// 	{id: 2, firstName: "Daniel", age: 21, address: "Detroit"}
+// ]
+
+// // console.log(listedArray[1].address)
+// listedArray.forEach((values, i) => {
+// 	console.log(`This individual has an id of ${values.id} at the index ${i} and their names is ${values.firstName}`)
+// })
+// console.log(obj.firstName);
+// console.log(obj["age"]);
+// console.log(obj.number);
+// console.log(hisName)
+// console.log(name2)
+
+
+// const sayHello = () => {
+// 	console.log("Hi how a")
+// }
+
+// const greet = (name) => {
+// 	console.log(`Hello ${name}`)
+// }
+
+
+// greet('HIE')
+
+// const isEven = num => num % 2 === 0
+
+// console.log(isEven(100))
+
+// export const getUser = (id) => {
+// 	const user = [
+// 		{ id: 1, firstName: "Nicholas", age: 24 },
+// 		{ id: 2, firstName: "Precious", age: 28 },
+// 		{ id: 3, firstName: "Daniel", age: 22 },
+// 	];
+// 	return user.find((user) => user.id === id);
+// };
+
+// const user = [
+// 	{ id: 1, firstName: "Nicholas", age: 24 },
+// 	{ id: 2, firstName: "Precious", age: 28 },
+// 	{ id: 3, firstName: "Daniel", age: 22 },
+// ];
+
+// user.forEach(user => {
+// 	console.log(`${user.id}`)
+// })
+
+
+for (let i = 0; i < 100; i++) {
+    console.log(i);
+}
---
+++ server.js
@@ -0,0 +1,121 @@
+// import express from "express"; // ES Modules style
+// import { getUser } from "./user.js";
+
+// const app = express();
+// const PORT = 3000;
+
+// // First endpoint: GET /users/:id
+// app.get("/users/:id", (req, res) => {
+// 	const userId = Number(req.params.id); // get id from URL
+// 	const user = getUser(userId); // get the user
+// 	if (!user) {
+// 		return res.status(404).json({ error: "User not found" });
+// 	}
+// 	res.json(user); // send user as JSON
+// });
+
+// // Start server
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+import express from "express"; // ES Modules style
+import { getUser } from "./user.js";
+
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+
+// This is the first path of the URL
+app.get("/", (req, res) => {
+	console.log("Testing something");
+	res.send(`Server running at http://localhost:${PORT}`);
+});
+
+app.get("/ping", (req, res) => {
+	res.json({ message: "pong" });
+});
+
+app.get("/status", (req, res) => {
+	res.json({
+		status: "ok",
+		uptime: "running",
+		version: "1.0.0",
+	});
+});
+
+// const notNumber = (x) => {
+// 	if
+
+// }
+
+app.get("/square/:number", (req, res) => {
+	const num = Number(req.params.number);
+	if (isNaN(num)) {
+		res.status(400).json({ error: "Not a number" });
+	}
+	res.json({
+		number: num,
+		square: num * num,
+	});
+});
+
+app.post("/users", (req, res) => {
+	console.log("POST /users - body:", req.body);
+	const userData = req.body;
+
+	res.json({
+		message: "Data received",
+		data: userData,
+	});
+});
+
+// Query params
+app.get("/users", (req, res) => {
+	const { age, country } = req.query;
+
+	res.json({
+		message: "message received",
+		age: age,
+		country: country,
+	});
+});
+
+// Path params
+app.get("/status/:num", (req, res) => {
+	const x = Number(req.params.num);
+
+	if (isNaN(x)) {
+		res.status(400).json({ error: "Not a number" });
+	}
+	res.json({
+		num: x,
+		addition: x + x,
+	});
+});
+
+const validate = (req, res, next) => {
+	const { email, password } = req.body;
+
+	if (!email || !password) {
+		return res.status(400).json({ error: "Enter something valid bitch" });
+	}
+
+	if (password.length < 6) {
+		return res.status(400).json({ error: "Enter a valid password bitch" });
+	}
+
+	next();
+};
+
+app.post("/logins", validate, (req, res) => {
+	const { email, password } = req.body;
+
+	res.json({
+		email: email,
+		password: password,
+	});
+});
+
+// Where the server listens for how request and port 3000
+app.listen(PORT, () => {
+	console.log(`Server running at http://localhost:${PORT}`);
+});
